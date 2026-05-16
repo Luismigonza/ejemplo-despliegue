249a1d54ccde573a3d3f1d556ce5755bb1a44abf
Luismigonza <luismigonzalez38@gmail.com>
2026-05-15 19:41:13 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ejemploDespliegue.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
